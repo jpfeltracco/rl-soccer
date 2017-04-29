8329d36f80780099cfc5e1686898b097de7ad515
Jeremy Feltracco <jpfeltracco@gatech.edu>
2017-04-28 21:21:35 -0400

    Move build to subdir of proto.

--- compile_proto.sh
+++ compile_proto.sh
@@ -2,5 +2,5 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SRC_DIR="$ROOT_DIR/proto"
-DST_DIR="$ROOT_DIR/build"
-protoc -I=$SRC_DIR --python_out=$DST_DIR $SRC_DIR/addressbook.proto
+DST_DIR="$ROOT_DIR/proto/build"
+protoc -I=$SRC_DIR --python_out=$DST_DIR $SRC_DIR/*
