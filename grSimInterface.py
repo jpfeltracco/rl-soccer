24ff04145dac0a5bf494ede1a56309730b8e3150
Jeremy Feltracco <jpfeltracco@gatech.edu>
2017-08-04 09:11:20 -0400

    Current laptop code, probably more updated?

--- grSimInterface.py
+++ grSimInterface.py
@@ -1,6 +1,9 @@
 import sys
 import socket
 import struct
+import time
+from threading import Thread
+from threading import Lock
 
 sys.path.append("proto/build")
 
@@ -12,34 +15,159 @@ import messages_robocup_ssl_geometry_pb2
 import messages_robocup_ssl_refbox_log_pb2
 import messages_robocup_ssl_wrapper_pb2
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
 
-def main():
+class RealTimeGrSim:
+    def run_rx(self):
+        #multicast_group = '224.5.23.2'
+        server_address = ('', 10020)
 
-    #multicast_group = '224.5.23.2'
-    server_address = ('', 10020)
+        # Create the socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Create the socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # Bind to the server address
-    sock.bind(server_address)
+        # Bind to the server address
+        sock.bind(server_address)
 
+        while not self.stop_flag:
+            data, addr = sock.recvfrom(1024)
+            wrapper = messages_robocup_ssl_wrapper_pb2.SSL_WrapperPacket()
 
-    while True:
-        data, addr = sock.recvfrom(1024)
-        wrapper = messages_robocup_ssl_wrapper_pb2.SSL_WrapperPacket()
-        try:
-            wrapper.ParseFromString(data)
-        except:
-            pass
-        for robot in wrapper.detection.robots_blue:
-            if (robot.robot_id == 1):
-                print(robot.x)
-                print(robot.y)
+            # unpack
+            with self.lock:
+                me_x, me_y, ball_x, ball_y = self.state
 
-    
+                try:
+                    wrapper.ParseFromString(data)
+
+                    for robot in wrapper.detection.robots_blue:
+                        if (robot.robot_id == 1):
+                            me_x = robot.x
+                            me_y = robot.y
+
+                    for ball in wrapper.detection.balls:
+                        ball_x = ball.x
+                        ball_y = ball.y
+
+                except:
+                    pass
+
+                # repack
+                self.state = (me_x, me_y, ball_x, ball_y)
+
+            time.sleep(0.001)
+
+    def run_tx(self):
+        send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        send_address = ('', 20011)
+
+        while not self.stop_flag:
+            packet = grSim_Packet_pb2.grSim_Packet()
+
+            with self.lock:
+                if self.reset_env:
+                    self.reset_env = False
+
+                    replace_packet = packet.replacement
+
+                    ball_replace = replace_packet.ball
+                    
+                    ball_replace.pos.x = 0.0
+                    ball_replace.pos.y = 0.0
+                    ball_replace.vel.x = 0.0
+                    ball_replace.vel.y = 0.0
+
+                    robot_replace = replace_packet.robots.add()
+                    robot_replace.x = -4.2
+                    robot_replace.y = 0.0
+                    robot_replace.dir = 0.0
+                    robot_replace.id = 1
+                    robot_replace.yellowteam = False
+
+            commands_packet = packet.commands
+            commands_packet.timestamp = time.time()
+            commands_packet.isteamyellow = 0
+
+            commands = packet.commands.robot_commands
+            c = commands.add()
+
+            vel_tan, vel_norm, vel_ang = self.vel_action
+
+            c.id = 1
+            c.kickspeedx = 0
+            c.kickspeedz = 0
+            c.veltangent = vel_tan
+            c.velnormal = vel_norm
+            c.velangular = vel_ang
+            c.spinner = False
+            c.wheelsspeed = False
+
+            send_sock.sendto(packet.SerializeToString(), send_address)
+
+            time.sleep(0.1)
+
+    def __init__(self):
+        self.lock = Lock()
+
+        # (me_x, me_y, ball_x, ball_y)
+        self.state = (0.0, 0.0, 0.0, 0.0)
+        # (tan, norm, ang)
+        self.vel_action = (0.0, 0.0, 0.0)
+
+        self.stop_flag = False
+        self.reset_env = False
+
+        self.rx_thread = Thread(target = self.run_rx)
+        self.tx_thread = Thread(target = self.run_tx)
+
+        self.rx_thread.start()
+        self.tx_thread.start()
+
+    def stop(self):
+        self.stop_flag = True
+
+    def is_done(self):
+        return time.time() - self.start_time > 5
+
+    def sample_reward(self):
+        # return some function robot and ball position
+        me_x, me_y, ball_x, ball_y = self.state
+        dst = pow(pow((me_x - ball_x) / 1000, 2) + pow((me_y - ball_y) / 1000, 2), 0.5)
+        return 1.0 if dst < 1.0 else 0.0
+
+    def set_current_action(self, vel_tan, vel_norm, vel_ang):
+        with self.lock:
+            self.vel_action = (vel_tan, vel_norm, vel_ang)
+
+    def get_current_state(self):
+        return self.state
+
+    def start(self):
+        self.start_time = time.time()
+
+    def reset(self):
+        with self.lock:
+            self.reset_env = True
+            self.vel_action = (0, 0, 0)
+
+        while self.reset_env:
+            time.sleep(0.1)
 
 if __name__ == "__main__":
-    main()
+    env = RealTimeGrSim()
+
+    time.sleep(0.5)
+
+    try:
+        while True:
+            env.reset() 
+            env.start()
+            while not env.is_done():
+                print(env.sample_reward())
+                env.set_current_action(1, 0, 0.01)
+                time.sleep(0.05)
+
+            env.reset() 
+    except KeyboardInterrupt:
+        env.stop()
+
--- policybased.py
+++ policybased.py
@@ -5,22 +5,14 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import math
 
-import gym
-env = gym.make("CartPole-v0")
+import time
 
-if False:
-    env.reset()
-    random_episodes = 0
-    reward_sum = 0
-    while random_episodes < 10:
-        env.render()
-        observation, reward, done, _ = env.step(env.action_space.sample())
-        reward_sum += reward
-        if done:
-            random_episodes += 1
-            print("Reward for this episode was: " + str(reward_sum))
-            reward_sum = 0
-            env.reset()
+from grSimInterface import RealTimeGrSim
+
+#import gym
+#env = gym.make("CartPole-v0")
+env = RealTimeGrSim()
+env.reset()
 
 H = 10 # number of hidden layer neurons
 batch_size = 5 # every how many episodes do param update?
@@ -36,14 +28,15 @@ observations = tf.placeholder(tf.float32, [None,D], name="input_x")
 W1 = tf.get_variable("W1", shape=[D, H],
                      initializer=tf.contrib.layers.xavier_initializer())
 layer1 = tf.nn.relu(tf.matmul(observations, W1))
-W2 = tf.get_variable("W2", shape=[H, 1],
+W2 = tf.get_variable("W2", shape=[H, 2],
                      initializer=tf.contrib.layers.xavier_initializer())
 score = tf.matmul(layer1, W2)
-probability = tf.nn.sigmoid(score)
+#probability = tf.nn.sigmoid(score) - 0.5
+probability = score
 
 # Define parts of the network needed for learning good policy
 tvars = tf.trainable_variables()
-input_y = tf.placeholder(tf.float32, [None,1], name="input_y")
+input_y = tf.placeholder(tf.float32, [None,2], name="input_y")
 advantages = tf.placeholder(tf.float32, name="reward_signal")
 
 # Loss function, sends weights in the direction of making actions
@@ -85,40 +78,54 @@ init = tf.initialize_all_variables()
 with tf.Session() as sess:
     rendering = False
     sess.run(init)
-    observation = env.reset() # Obtain an initial observation of the environment
+    observation = env.get_current_state() # Obtain an initial observation of the environment
+    print(observation)
     # Reset the gradient placeholder. We will collect gradients in gradBuffer
     # until we are ready to update our policy network.
     gradBuffer = sess.run(tvars)
     for ix,grad in enumerate(gradBuffer):
         gradBuffer[ix] = grad * 0
 
+    env.start()
     while episode_number <= total_episodes:
-
-        good_agent = running_reward is not None and running_reward/batch_size > 190
-        # it once our agent is doing a good job.
-        if good_agent or rendering:
-            env.render()
-            rendering = True
-
         # Make sure the observation is in the shape the network can handle
         x = np.reshape(observation, [1, D])
+        x.itemset(0, x.item(0) / 9000)
+        x.itemset(1, x.item(1) / 6000)
+        x.itemset(2, x.item(2) / 9000)
+        x.itemset(3, x.item(3) / 6000)
 
         # Run the policy network and get an action to take.
-        tfprob = sess.run(probability, feed_dict={observations: x})
-        action = 1 if np.random.uniform() < tfprob else 0
+        output = sess.run(probability, feed_dict={observations: x})
+        print(output)
+        if np.random.uniform() < .10:
+            output.itemset(0, np.random.uniform() - 0.5)
+            output.itemset(1, np.random.uniform() - 0.5)
+
+        #print(output)
+        #action = 1 if np.random.uniform() < tfprob else 0
+        #action = 1 if np.random.uniform() < tfprob else 0
         xs.append(x)
-        y = 1 if action == 0 else 0
-        ys.append(y)
+        #y = 1 if action == 0 else 0
+        ys.append(output + 0.00001)
+
+        #env.set_current_action(output.item(0), output.item(1), output.item(2))
+        env.set_current_action(output.item(0) * 5.0, output.item(1) * 5.0, 0)
 
         # step the environment and get new measurements
-        observation, reward, done, info = env.step(action)
+        #observation, reward, done, info = env.step(action)
+        observation = env.get_current_state()
+        reward = env.sample_reward()
+        print(reward)
+
         reward_sum += reward
         # record reward(has to be done after we call step() to get reward
         # for previous actions)
-        drs.append(reward)
+        drs.append(reward + 0.00001)
 
-        if not good_agent and done:
+        if env.is_done():
             episode_number += 1
+            #print("episode: " + str(episode_number))
             # stack together all inputs, hidden states, action gradients, and rewards
             # for this episode
             epx = np.vstack(xs)
@@ -135,6 +142,8 @@ with tf.Session() as sess:
             discounted_epr /= np.std(discounted_epr)
 
             # Get the gradient for this episode, and save it in the gradbuffer
+            print(epx)
+            print(epy)
             tGrad = sess.run(newGrads, feed_dict={observations: epx, input_y: epy, advantages: discounted_epr})
             for ix, grad in enumerate(tGrad):
                 gradBuffer[ix] += grad
@@ -149,12 +158,15 @@ with tf.Session() as sess:
                 running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
                 print("Average reward for episode: " + str(reward_sum / batch_size) + " total average reward: " + str(running_reward/batch_size))
 
-                if reward_sum/batch_size > 200:
-                    print("Task solved in " + str(episode_number) + " episodes!")
-                    break
+                
+                #if reward_sum/batch_size > 200:
+                #    print("Task solved in " + str(episode_number) + " episodes!")
+                #    break
 
                 reward_sum = 0
 
-            observation = env.reset()
+            #observation = env.reset()
+            env.reset()
+            env.start()
 
 print(str(episode_number) + " episodes completed.")
